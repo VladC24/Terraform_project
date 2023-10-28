5e87cd34c56efba89369e00d491aa0171fdb4241
Vlad A Chiriac <vladc.@vlads-mbp.home>
2023-10-28 14:36:40 +0100

    Add vars files for dev, testr and prod envs

---
+++ module/main.tf
@@ -0,0 +1,22 @@
+variable "region" {
+    type = string
+    default = "eu-west-2"
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "number_of_servers" {
+    type = number
+}
+
+provider "aws" {
+    region = var.region
+}
+
+module "ec2" {
+    source = "./ec2"
+    count = var.environment == "test" ? 2 : 1
+}
+
