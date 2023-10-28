b8faad370c3445f6999b6333020f7cc34aeabb2b
Vlad A Chiriac <vladc.@vlads-mbp.home>
2023-10-28 19:11:34 +0100

    Change the ingres and egress to be dynamic

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -1,3 +1,13 @@
+variable "ingressrules" {
+    type = list(number)
+    default = [80, 443]
+}
+
+variable "egressrules" {
+    type = list(number)
+    default = [80, 443, 25, 3306, 53, 8080]
+}
+
 resource "aws_instance" "ec2" {
     ami = "ami-032598fcc7e9d1c7a"
     instance_type = "t2.micro"
@@ -10,17 +20,25 @@ resource "aws_eip" "elasticip" {
 
 resource "aws_security_group" "webtraffic" {
     name = "Allow HTTPS"
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
+    dynamic "ingress" {
+        iterator = port
+        for_each = var.ingressrules
+        content {
+            from_port = port.value
+            to_port = port.value
+            protocol = "TCP"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
     }
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
+    dynamic "egress" {
+        iterator = port
+        for_each = var.egressrules
+        content {
+            from_port = port.value
+            to_port = port.value
+            protocol = "TCP"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
     }
 }
 
